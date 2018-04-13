a654d4f4d073af2ef60711e05be4a8f8f1923ed4
baa7968 <baa7968@rit.edu>
2018-04-13 19:38:30 -0400

    update

--- killpross.sh
+++ killpross.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-while :
+mytty=$(tty | awk -F "/" '{print $4}')
+echo my tty $mytty
+for i in {0..30}
 do
-	mytty=$(tty | awk -F "/" '{print $4}')
-	echo my tty $mytty
-	for i in {0..30}
-	do
-		echo $i
- 		if [[ $mytty == $i ]]; then
-			continue
-		else
-			bad=$(ps -aux | pgrep -f pts/$i)
-			echo $bad
-			kill $bad
-		fi
-	/tmp/two.sh
-done
+	echo $i
+ 	if [[ $mytty == $i ]]; then
+		continue
+	else
+		bad=$(ps -aux | pgrep -f pts/$i)
+		echo $bad
+		kill $bad
+	fi
+/tmp/two.sh
